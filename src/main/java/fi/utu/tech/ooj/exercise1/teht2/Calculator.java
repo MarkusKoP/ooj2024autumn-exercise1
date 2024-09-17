76fe57057089c445b0ec9ffd2a31553f9786528a
MarKop <markus.r.koponen@utu.fi>
2024-09-17 14:42:05 +0300

    Korjattu Calculator

--- src/main/java/fi/utu/tech/ooj/exercise1/teht2/Calculator.java
+++ src/main/java/fi/utu/tech/ooj/exercise1/teht2/Calculator.java
@@ -1,12 +1,30 @@
 package fi.utu.tech.ooj.exercise1.teht2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Calculator {
     public static float avg(int[] nums) throws EmptyArrayException {
-        int sum = 0;
         if (nums == null || nums.length == 0)
             throw new EmptyArrayException();
-        for(int n: nums) sum += n;
-        return sum / nums.length;
+        }
+
+        static int summa = 0;
+        List<String> negatiiviset = new ArrayList<>(); // alustukset
+        boolean virheitä = false;
+
+        for (int indeksi = 0; indeksi < nums.length; indeksi++) { // läpikäynti
+            if (nums[indeksi] < 0){ // onko negatiivinen
+                String virhe = ("Antamasi taulukon" + (indeksi+ 1) + "luku" + nums[indeksi] + "on negatiivinen.");
+                negatiiviset.add(virhe); // tallennetaan virhe viesti negatiivisesta
+                virheitä = true;
+            }
+            else {
+                summa += nums[indeksi];
+            }
+        if (virheitä){
+            throw new IllegalArgumentException(String.join("\n", negatiiviset)); // tarkistettu tekoälyltä syntaksi
+        }
+        }
+        return (float) summa / nums.length;
     }
-}
-// Testi
